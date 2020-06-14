e9e9762c95b41d9cc11628887465fcfc4aa3febb
Corey Post <corey.post0120@gmail.com>
2020-06-13 22:50:54 -0600

    init set up for program

---
+++ assets/quiz.js
@@ -0,0 +1,20 @@
+// global variables
+
+
+
+// countdown timer in seconds
+
+
+
+
+// what happens when you click an answer
+// timer starts(run timer after first click to start)
+
+
+
+//What happens when timer reaches zero or if all questions are answered
+
+
+
+
+//store top score with initials in storage
