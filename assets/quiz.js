a64429bd428e0e567848491e1358f0ecfd7511f4
Corey Post <corey.post0120@gmail.com>
2020-06-15 23:55:00 -0600

    added variables to javascript

--- assets/quiz.js
+++ assets/quiz.js
@@ -1,6 +1,40 @@
 // global variables
+var startBtn = document.getElementById("startBtn");
+var submitBtn = document.getElementById("submitBtn");
+var timer = document.getElementById("timer");
+var userScore = document.getElementById("userScore");
+var questionBody = document.getElementById("questions");
+var answers = document.getElementById("answers");
+var submitScore = document.querySelector("#submitScore");
 
-
+// questions, choices, and answers in an array
+var questions = [
+    {
+        title: "Which tag do you insert the HTML to link the javascript?",
+        options: ["<script>","<java>","<javascript>","<js>"],
+        answer: "<script>"
+    },
+    {
+        title: "What type of pop up box will allow a user to type a response?",
+        options: ["Confirm","Alert","Prompt","Input"],
+        answer: "Prompt"
+    },
+    {
+        title: "The condition of an if/else statement is enclosed within ___.",
+        options: ["square brackets","curly brackets","quotations","parentheses"],
+        answer: "parentheses"
+    },
+    {
+        title: "Commonly Used Data Types DO NOT include:",
+        options: ["strings","alerts","booleans","numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "Arrays in Javascript can be used to store?",
+        options: ["Other Arrays","Strings and Numbers","Booleans","All of the Above"],
+        answer: "All of the Above"
+    }
+];
 
 // countdown timer in seconds
 
