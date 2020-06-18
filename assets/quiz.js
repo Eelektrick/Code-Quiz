68c45ad4a6fd75fb9772773c4488a6731a60ade9
Corey Post <corey.post0120@gmail.com>
2020-06-18 00:46:28 -0600

    added function to work timer and make questions appear

--- assets/quiz.js
+++ assets/quiz.js
@@ -1,11 +1,30 @@
 // global variables
 var startBtn = document.getElementById("startBtn");
 var submitBtn = document.getElementById("submitBtn");
-var timer = document.getElementById("timer");
+var timerEl = document.getElementById("timer");
 var userScore = document.getElementById("userScore");
 var questionBody = document.getElementById("questions");
 var answers = document.getElementById("answers");
 var submitScore = document.querySelector("#submitScore");
+
+var questionNumber = -1;
+var answer;
+var index = 0;
+
+// Start the quiz with a click of the start quiz button
+startBtn.addEventListener("click", startQuiz);
+
+//function that operates the entire quiz
+function startQuiz(){
+    //swap welcome message to the quiz
+    document.getElementById("home").classList.add("d-none");
+    document.getElementById("quiz").classList.remove("d-none");
+
+    //timer begins at 75 seconds
+    timer();
+    // create questions and display on screen
+    makeQuestions();
+}
 
 // questions, choices, and answers in an array
 var questions = [
@@ -36,19 +55,45 @@ var questions = [
     }
 ];
 
-// countdown timer in seconds
+// countdown timer in seconds and ending when timer hits 0.
+function timer(){
+    var secondsLeft = 75;
 
+    var timerInterval = setInterval(function(){
+    timerEl.textContent = "Time: " + secondsLeft;
+    secondsLeft--;
 
+    if(secondsLeft === 0){
+        clearInterval(timerInterval);
+        finalResults();
+    }
 
+    },1000);
+}
+
+// make questions appear when hitting the start button
+function makeQuestions(){
+    questionNumber++;
+    answer = questions[questionNumber].answer;
+
+    questionBody.textContent = questions[questionNumber].title;
+    answers.innerHTML = "";
+
+    var option = questions[questionNumber].options;
+
+    for(var i=0; i<option.length; i++){
+        var nextOption = document.createElement("button");
+
+        nextOption.textContent = option[i];
+        answerBtn = answers.appendChild(nextOption).setAttribute("class", "p-3 m-1 btn-outline-danger btn-block");
+    }
+}
+
+// finalResults(){
+
+// }
 
 // what happens when you click an answer
 // timer starts(run timer after first click to start)
-
-
-
-//What happens when timer reaches zero or if all questions are answered
-
-
-
 
 //store top score with initials in storage
